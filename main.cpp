828e84bb4ba5d5fa8e70fde5404ff50a9c4d424e
Andreas Weis <der_ghulbus@ghulbus-inc.de>
2023-09-28 15:53:45 +0200

    mixed modules/include standard library example

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <string>
+
+import m;
+
+
+int main() {
+    std::string str = get_string();
+    return static_cast<int>(str.length());
+}
---
+++ module.cpp
@@ -0,0 +1,7 @@
+export module m;
+
+import std;
+
+export std::string get_string() {
+    return "Hello from Module";
+}
